1f21d02c8adf5a76b93ba12347bd592c81b03836
darthgnu <darthgnu@d7b07571-c7f4-0310-acde-8fe97a66e80c>
2005-04-28 18:49:26 +0000

    Added support .c file with register arguments.

---
+++ c54x/c54x.c
@@ -0,0 +1,28 @@
+void
+init_cumulative_args (CUMULATIVE_ARGS *cum, tree fntype, rtx libname, tree fndecl)
+{
+	cum->numarg = 0;
+	cum->has_varargs = false;
+
+	tree param, next_param;
+
+	for (param = (fntype) ? TYPE_ARG_TYPES (fntype) : 0; param != 0; param = next_param) {
+		next_param = TREE_CHAIN (param);
+		if (next_param == 0 && TREE_VALUE (param) != void_type_node) {
+			/* If the last parameter is not of type void_type_node, we have a variadic function */
+			cum->has_vaargs = true;
+		}
+	}
+}
+
+rtx
+function_arg (CUMULATIVE_ARGS *cum, enum machine_mode mode, tree type, int named)
+{
+	rtx ret = NULL_RTX;
+
+	if(cum->numarg == 0 && mode != VOIDmode && cum->has_varargs == false) {
+		ret = gen_rtx_REG(mode, A_REGNO);
+	}
+
+	return ret;
+}
