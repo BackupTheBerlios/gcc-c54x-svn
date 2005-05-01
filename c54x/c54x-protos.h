2089d4af8dbc9da64b1367bdffcb9ae59b88bc48
darthgnu <darthgnu@d7b07571-c7f4-0310-acde-8fe97a66e80c>
2005-05-01 21:01:27 +0000

    Added prototypes file.

---
+++ c54x/c54x-protos.h
@@ -0,0 +1,5 @@
+extern int c54x_hard_regno_mode_ok (unsigned int, enum machine_mode);
+extern void init_cumulative_args (CUMULATIVE_ARGS*, tree, rtx, tree);
+extern rtx function_arg (CUMULATIVE_ARGS*, enum machine_mode, tree, int);
+extern void function_arg_advance (CUMULATIVE_ARGS *, enum machine_mode, tree, int);
+	 
