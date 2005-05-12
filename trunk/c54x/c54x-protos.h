4a919f3576a7a09a6e5ef96b995cbb5c17665368
darthgnu <darthgnu@d7b07571-c7f4-0310-acde-8fe97a66e80c>
2005-05-12 22:08:06 +0000

    Added copyright notices.

--- trunk/c54x/c54x-protos.h
+++ trunk/c54x/c54x-protos.h
@@ -1,3 +1,24 @@
+/* Definitions of target machine for GCC for the Texas Instruments TMS320C54x
+   Copyright (C) 2005 Free Software Foundation, Inc.
+   Contributed by Bryan Richter and Jonathan Bastien-Filiatrault
+
+This file is part of GCC.
+
+GCC is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+GCC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GCC; see the file COPYING.  If not, write to
+the Free Software Foundation, 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.  */
+
 extern int c54x_hard_regno_mode_ok (unsigned int, enum machine_mode);
 extern void init_cumulative_args (CUMULATIVE_ARGS*, tree, rtx, tree);
 extern rtx function_arg (CUMULATIVE_ARGS*, enum machine_mode, tree, int);
